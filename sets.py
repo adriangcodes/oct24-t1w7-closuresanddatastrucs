9aa5b5efdabc2b44666a194f26b3a842101715a7
Adrian Gidaro <adriangidaro@gmail.com>
2024-12-01 14:56:38 +1100

    Finished sets

--- sets.py
+++ sets.py
@@ -1,1 +1,20 @@
-# List - array -  ordered (indexed) collection of items - mutable
+# List - array -  ordered (indexed) collection of items - mutable, can have duplicates
+
+# Set - unordered - not indexed - mutable
+
+names_set = {'John', 'Jane', 'Stacy', 'Luke'}
+
+# print(names_set)
+# print(type(names_set))
+# print(len(names_set)) # length
+
+# print(names_set(1)) # will produce error - sets are unordered
+
+names_set.add('Mary')
+print(names_set)
+
+names_set.remove('John')
+print(names_set)
+
+names_set.add('Jane') # has no effect because Jane already exists in the set
+print(names_set)
