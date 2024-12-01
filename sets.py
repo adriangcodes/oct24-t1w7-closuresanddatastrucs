1c753bafe89d3770b0273037da7b13efbdd3086e
Adrian Gidaro <adriangidaro@gmail.com>
2024-12-01 15:09:07 +1100

    Finished sets

--- sets.py
+++ sets.py
@@ -18,3 +18,11 @@ print(names_set)
 
 names_set.add('Jane') # has no effect because Jane already exists in the set
 print(names_set)
+
+set1 = {1, 2, 3, 4, 5}
+set2 = { 1, 3, 5, 7, 9}
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
+print(set2 - set1)
